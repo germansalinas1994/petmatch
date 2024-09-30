60a172e7932d1c5253fa6c632e54fb64c8cba99e
German Salinas <germansalinas.fce@gmail.com>
2024-09-30 14:22:15 -0300

    agrego variables de entorno

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -2,9 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function home() {
+  const prueba = process.env.EXPO_PUBLIC_VARIABLE;
+  console.log(prueba);
+
   return (
     <View>
       <Text>Home</Text>
+      <Text>{prueba}</Text>
     </View>
   )
 }
