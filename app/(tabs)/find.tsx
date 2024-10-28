487bc924941a493d9e7c84ea0ed4fe016ceed162
German Salinas <germansalinas.fce@gmail.com>
2024-10-28 00:14:00 -0300

    componente para botones de like o dislike

--- app/(tabs)/find.tsx
+++ app/(tabs)/find.tsx
@@ -1,87 +1,75 @@
-import { View, Image, Dimensions, Pressable, Text } from "react-native";
+import {
+  View,
+  Image,
+  Dimensions,
+  Pressable,
+  Text,
+  StyleSheet,
+  SafeAreaView
+} from "react-native";
 import React from "react";
 import Colors from "../../constants/Colors";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import Icon from "react-native-vector-icons/FontAwesome";
 import { Link } from "expo-router";
+import Buttons from "@/components/find/Buttons";
 
 export default function Find() {
   const { width, height } = Dimensions.get("window");
   const insets = useSafeAreaInsets();
 
+  const handleLike = () => {
+    console.log("Liked!");
+    alert("Liked!");
+    // lógica para manejar el like
+  };
+
+  const handleDislike = () => {
+    console.log("Disliked!");
+    alert("Disliked!");
+    // lógica para manejar el dislike
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: Colors.background.paper,
-        paddingTop: insets.top,
-        paddingBottom: insets.bottom,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {/* Contenedor de la imagen */}
-      <Link href="/pet-details" asChild>
-        <Pressable
-          style={{
-            width: width * 1,
-            height: height * 0.7,
-            overflow: "hidden",
-            alignItems: "center",
-            justifyContent: "center",
-            shadowColor: Colors.text.primary,
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.8,
-            shadowRadius: 5,
-            elevation: 5,
-          }}
-        >
-          <Image
-            source={require("../../assets/images/mascota1.jpeg")}
-            resizeMode="cover"
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-          />
-        </Pressable>
-      </Link>
-
-      {/* Contenedor de botones */}
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: width * 0.6,
-          marginTop: 20,
-        }}
-      >
-        <Pressable
-          style={{
-            backgroundColor: Colors.error.main,
-            width: 60,
-            height: 60,
-            borderRadius: 30,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Icon name="times" size={30} color={Colors.text.white} />
-        </Pressable>
-
-        <Pressable
-          style={{
-            backgroundColor: Colors.success.main,
-            width: 60,
-            height: 60,
-            borderRadius: 30,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Icon name="check" size={30} color={Colors.text.white} />
-        </Pressable>
+    <SafeAreaView style={[styles.safeArea, { paddingTop: insets.top }]}>
+      <View style={styles.container}>
+        <Link href="/pet-details" asChild>
+          <Pressable style={styles.imageContainer}>
+            <Image
+              source={require("../../assets/images/mascota1.jpeg")}
+              resizeMode="cover"
+              style={styles.image}
+            />
+          </Pressable>
+        </Link>
+        <Buttons onLike={handleLike} onDislike={handleDislike} />
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: Colors.background.paper,
+  },
+  container: {
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: "100%",
+    height: Dimensions.get("window").height * 0.7,
+    overflow: "hidden",
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: Colors.text.primary,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 8,
+    elevation: 5,
+    marginVertical: 20,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  }
+});
