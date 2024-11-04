a24b4284f4420da752d5b0d33fa3b8e587479c98
German Salinas <germansalinas.fce@gmail.com>
2024-11-04 11:52:05 -0300

    manejo de la navegacion desde app/layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,8 @@
-import { Stack, useRouter } from "expo-router";
-import { useEffect } from "react";
-import useUserStore from "@/stores/userStore"; // Ajusta la ruta según la ubicación de tu store
-import { RoleCodes } from "@/constants/roles"; // Ajusta la ruta según la estructura de constantes
-
+import { Stack } from "expo-router";
 export default function GeneralTabsLayout() {
-  const router = useRouter();
-  const { codigoRol } = useUserStore();
 
-  useEffect(() => {
-    // Redirige al layout correspondiente basado en el rol
-    if (codigoRol === RoleCodes.Adoptante) {
-      router.replace("/(tabs)/adoptante");
-    } else if (codigoRol === RoleCodes.Rescatista) {
-      router.replace("/(tabs)/rescatista");
-    }
-  }, [codigoRol, router]);
+
+
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
