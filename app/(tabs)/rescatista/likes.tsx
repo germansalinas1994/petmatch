06d702fc2631bbd3ae11e5abd27b5f4ba507dc7b
Tomas Pipolo <tpipolo.corporate@gmail.com>
2024-11-15 19:46:38 -0300

    Subo ultimos cambios

--- app/(tabs)/rescatista/likes.tsx
+++ app/(tabs)/rescatista/likes.tsx
@@ -186,7 +186,8 @@ export default function Likes() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // Asegura que el contenedor ocupe toda la pantalla
+    flex: 1,
+    paddingTop: 20, // Asegura que el contenedor ocupe toda la pantalla
   },
   userCard: {
     flexDirection: "row",
