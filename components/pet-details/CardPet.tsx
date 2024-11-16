ba2a0abee4395755cb40b433181eacedf293df51
tomaspipolo1 <tpipolo.corporate@gmail.com>
2024-11-15 21:17:20 -0300

    Modifique el form de editar para el crop de las imagenes

--- components/pet-details/CardPet.tsx
+++ components/pet-details/CardPet.tsx
@@ -58,7 +58,7 @@ const styles = StyleSheet.create({
     },
     image: {
         width: "100%",
-        height: 350,
+        height: 400,
         borderTopLeftRadius: 8,
         borderTopRightRadius: 8,
     },
